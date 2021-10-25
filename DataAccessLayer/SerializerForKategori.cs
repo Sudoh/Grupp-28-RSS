2cff7713193564137c627cff5563f868f0458ba1
Amira-sawas <91065425+Amira-sawas@users.noreply.github.com>
2021-10-25 16:40:02 +0200

    ddd
    ddd

---
+++ DataAccessLayer/SerializerForKategori.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using modelss;
+using DataAccesLayer.Exceptions;
+
+
+namespace DataAccesLayer
+{
+    internal class SerializerForKategori
+    {
+        public void Serialize(List<Kategori> personList)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Kategori>));
+                using (FileStream outFile = new FileStream("Kategori.xml", FileMode.Create,
+                    FileAccess.Write))
+                {
+                    xmlSerializer.Serialize(outFile, personList);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new SerializerException("Kategori.xml", "Could not serialize to the file");
+            }
+        }
+
+        public List<Kategori> Deserialize()
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Kategori>));
+                using (FileStream inFile = new FileStream("Kategori.xml", FileMode.Open,
+                    FileAccess.Read))
+                {
+                    return (List<Kategori>)xmlSerializer.Deserialize(inFile);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new SerializerException("Kategori.xml", "Could not deserialize the file.");
+            }
+            
+        }
+    }
+}
